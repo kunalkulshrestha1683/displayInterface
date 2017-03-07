fdccea444c44fd2ff46e6d9ac37bd4a0e02c81b0
kunal kulshrestha <kunalkulshrestha1683@gmail.com>
2017-03-07 15:22:47 +0530

    complete source

---
+++ linux-imx/3.14.28-r0/image/usr/src/kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Jan 4 19:26:55 IST 2017"
+#define LINUX_COMPILE_BY "kunalk"
+#define LINUX_COMPILE_HOST "Linux2-tds"
+#define LINUX_COMPILER "gcc version 4.9.1 (GCC) "
---
+++ linux-imx/3.14.28-r0/sysroot-destdir/usr/src/kernel/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.14.28-1.0.0_ga+g91cf351"
